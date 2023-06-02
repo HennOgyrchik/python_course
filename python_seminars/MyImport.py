964c25ca3225a8f46497750cc0de2f96be70c53a
Henn <HennOgyrchik@gmail.com>
2023-06-02 21:51:31 +0300

    hw 5

---
+++ python_seminars/MyImport.py
@@ -0,0 +1,18 @@
+def MyPow (a,b):
+    if b==1:
+        return a
+    elif b==-1:
+        return 1/a
+    elif b>1:    
+        return a * MyPow(a,b-1)
+    else:
+        return 1/a * MyPow(a, b+1)
+    
+
+def MySum (a,b):
+    if a!=0:
+        return MySum(a-1,b)+1
+    elif b!=0:
+        return MySum(a,b-1)+1
+    else:
+        return 0
